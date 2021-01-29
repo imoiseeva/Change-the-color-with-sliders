f89456e4af29a410309f8641c039387f0dfd65a6
imoiseeva <moiseeva.irina@gmail.com>
2021-01-29 12:26:16 +0200

    Initial Commit

---
+++ Change the color with sliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Change the color with sliders
+//
+//  Created by Irina Moiseeva on 29.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
